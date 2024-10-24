ea3ee1dae3f10f2af1c27a305b5a57f2f6aff10f
Jens Pots <jens@jenspots.com>
2024-10-24 20:11:44 +0200

    feat: migrated from monorepo

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+  kotlin("jvm") version "2.0.21"
+  id("co.uzzu.dotenv.gradle") version "4.0.0"
+}
+
+group = "technology.idlab"
+
+version = "0.0.1"
+
+kotlin { jvmToolchain(22) }
+
+repositories {
+  mavenCentral()
+
+  maven {
+    url = uri("https://maven.pkg.github.com/rdf-connect/orchestrator")
+    credentials {
+      username = env.fetchOrNull("GITHUB_ACTOR") ?: System.getenv("GITHUB_ACTOR")
+      password = env.fetchOrNull("GITHUB_TOKEN") ?: System.getenv("GITHUB_TOKEN")
+    }
+  }
+}
+
+dependencies {
+  // Kotlin extensions
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+
+  // RDFC SDK
+  implementation("technology.idlab:rdfc-core")
+  implementation("technology.idlab:rdfc-processor")
+
+  // RDF dependencies.
+  implementation("org.apache.jena:apache-jena-libs:5.0.0")
+  implementation("org.apache.jena:jena-arq:5.0.0")
+  implementation("org.apache.jena:jena-shacl:5.0.0")
+
+  // KTest
+  testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+  useJUnitPlatform()
+
+  testLogging { showStandardStreams = true }
+}
